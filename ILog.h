8904522ffed93819b0dbdb7f108df9882ace1f45
Madalin Morcov <madalinmorcov@gmail.com>
2017-09-06 10:17:50 +0300

    Create ILog.h

---
+++ ILog.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <ctime>
+
+enum class ErrorType
+{
+	FATAL,
+	ERROR,
+	WARNING,
+	INFO,
+	DEBUG,
+	ENUMEND
+};
+
+class ILog
+{
+public:
+	ILog();
+	virtual void Log(const std::string& message)=0;
+	~ILog();
+	
+private:
+	std::ofstream logOutput;
+	time_t m_timer;
+	
+
+};
+
